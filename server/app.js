ebad5124f302f5c88ca34ac090e481a22c17513d
partorican <ibringwater@gmail.com>
2019-10-01 21:29:13 -0600

    another push with more dependencies

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 var React = require('react')
+var $ = require("jquery");
 
 app.get('/', (req, res) => res.send('HELLO world!'));
 
